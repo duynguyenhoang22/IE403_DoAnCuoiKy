bd462892248a9f810d4f7396fd152dcdfbec7695
22520391TranNguyenNamHai <hnam0813@gmail.com>
2026-03-30 13:25:14 +0700

    gen data 0

--- data/gen_label_0.py
+++ data/gen_label_0.py
@@ -45,7 +45,7 @@ import pandas as pd
 # =============================================================================
 load_dotenv()
 API_KEY    = os.getenv("GEMINI_API_KEY", "")
-MODEL_NAME = "gemini-3-flash-preview"
+MODEL_NAME = "gemini-2.5-flash"
 
 OUTPUT_FILE           = "synthetic_data/synthetic_legitimate_label0.csv"
 TOTAL_SAMPLES         = 3000
